6a7ab3b4dd206a42b54a36bcfcb7ca37dc7230fa
btesfay8 <blenafomya@gmail.com>
2024-10-15 14:15:59 -0400

    testing

--- BudgetTracker/ContentView.swift
+++ BudgetTracker/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "pencil.circle")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
