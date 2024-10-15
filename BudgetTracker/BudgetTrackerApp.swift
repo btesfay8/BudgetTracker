188730fe3c7cc56b75a955a97495ac46c500d0dc
btesfay8 <blenafomya@gmail.com>
2024-10-15 14:14:37 -0400

    Initial Commit

---
+++ BudgetTracker/BudgetTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetTrackerApp.swift
+//  BudgetTracker
+//
+//  Created by Blen Tesfaye on 10/15/24.
+//
+
+import SwiftUI
+
+@main
+struct BudgetTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
