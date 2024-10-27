49a62213976782cd9d91e9f078a2d683936b94d5
BriaWilliams08 <briawilliams827@gmail.com>
2024-10-27 13:56:44 -0400

    add expense

---
+++ BudgetTracker/Expense.swift
@@ -0,0 +1,16 @@
+//
+//  Expense.swift
+//  BudgetTracker
+//
+//  Created by Bria Williams on 10/22/24.
+//
+
+import Foundation
+
+struct Expense : Identifiable {
+    let id = UUID()
+    var name : String
+    var amount: Double
+    var category: String
+}
+
