6031d19ede064e988cd78d1e479e2feb768b25f0
BriaWilliams08 <briawilliams827@gmail.com>
2024-10-27 14:16:49 -0400

    add expense view working

--- BudgetTracker/AddExpenseView.swift
+++ BudgetTracker/AddExpenseView.swift
@@ -8,22 +8,21 @@
 import SwiftUI
 
 struct AddExpenseView: View {
-    @Binding var expenses: [Expense] = []
+    @Binding var expenses: [Expense]
     @State private var name: String = ""
     @State private var amount: String = ""
     @State private var selectedCategory: String = ""
     let categories = ["Food", "Entertainment", "Housing", "Transportation", "Misc"]
-    @Environment(\.presentationMode) var presentationMode //may be deprecated
+    @Environment(\.dismiss) var dismiss
     
     var body: some View {
         NavigationView{
             Form{ //typically for data entry
-                Section(header: Text("idk what to put here yet")){
-                    TextField("Name", text: $name) //editable text interface
-                    //TODO figure out how to format to currency
+                Section(header: Text("Enter Expense Details")){
+                    TextField("Description", text: $name)
                     TextField("Amount", text: $amount)
-                        .keyboardType(.decimalPad) //only numbers and decimal point
-                    Picker("Category", selections: $selectedCategory) {
+                        .keyboardType(.decimalPad) //only numbers and decimal points                        }
+                    Picker("Category", selection: $selectedCategory) {
                         ForEach(categories, id: \.self) {cat in
                             Text(cat).tag(cat)
                         }
@@ -34,17 +33,17 @@ struct AddExpenseView: View {
             }
             .navigationTitle("Add Expense")
             .navigationBarItems(leading: Button("Cancel") {
-                presentationMode.wrappedValue.dismiss()
+                dismiss()
             }, trailing: Button("Submit") {
                 if let amountValue = Double(amount) {
-                    let newExpense = Expense(name: name, amount: amountValue, category: selectedCategoryategory)
+                    let newExpense = Expense(name: name, amount: amountValue, category: selectedCategory)
                     expenses.append(newExpense)
-                    presentationMode.wrappedValue.dismiss() //look up and comment this
+                    dismiss() //look up and comment this
                 }
             })
         }
     }
 }
 #Preview {
-    AddExpenseView()
+    AddExpenseView(expenses: .constant([]))
 }
