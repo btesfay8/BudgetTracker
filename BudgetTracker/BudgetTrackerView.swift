943699fe6e69f9099b856682f1e74194f5ee546c
BriaWilliams08 <briawilliams827@gmail.com>
2024-10-27 14:50:42 -0400

    add button added to budgetTrackerView

--- BudgetTracker/BudgetTrackerView.swift
+++ BudgetTracker/BudgetTrackerView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct BudgetTrackerView: View {
     // A list of categories
     let categories = ["Entertainment", "Food", "Housing"]
+    @State private var expenses: [Expense] = []
     
     // State variable to hold the selected category
     @State private var selectedCategory = "Entertainment"
@@ -33,7 +34,22 @@ struct BudgetTrackerView: View {
                 .font(.headline)
                 .padding()
             
-            Spacer()
+            Spacer() //button on bottom
+            
+            HStack{
+                NavigationLink(destination: AddExpenseView(expenses: $expenses)) {
+                    Text("Add")
+                        .font(.title)
+                        .padding()
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(Color.orange)
+                        .cornerRadius(20)
+                }
+                .padding(.leading, 50)
+                Spacer()
+            }
+            
         }
         .padding()
     }
